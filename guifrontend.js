69d7c4af335d095ed123e04ed6aed34510a6bd5d
Radosław Biereta <116495178+RadoslawBiereta@users.noreply.github.com>
2022-11-26 21:12:04 +0100

    Formatowanie kodu

--- guifrontend.js
+++ guifrontend.js
@@ -4,11 +4,13 @@ const port = 80;
 const app = express();
 app.use(express.static('views/pages'))
 app.set('view engine', 'ejs');
-app.get("/docs", function(req, res) {res.render("pages/docs", {});});
+app.get("/docs", function(req, res) {
+    res.render("pages/docs", {});
+});
 app.get("/", function(req, res1) {
     const http = require('http');
     let request = http.get('http://localhost:5000/getServerDate', (res) => {
-        let data ='';
+        let data = '';
         if (res.statusCode !== 200) {
             console.error('Did not get an OK from the server.');
             res.resume();
@@ -18,17 +20,20 @@ app.get("/", function(req, res1) {
             data += chunk;
         });
         res.on('close', () => {
- 	    var data1 = returnDate();
+            var data1 = returnDate();
             console.log('Frontend date: ' + data1);
             console.log('Backend date: ' + data);
-            res1.render("pages/index", {serverdate:data, frontenddate: data1});
+            res1.render("pages/index", {
+                serverdate: data,
+                frontenddate: data1
+            });
         });
     });
 });
 app.get("/getSampleText4Tests", function(req, res1) {
     const http = require('http');
     let request = http.get('http://localhost:5000/getSampleText4Tests', (res) => {
-        let data ='';
+        let data = '';
         if (res.statusCode !== 200) {
             console.error('Did not get an OK from the server.');
             res.resume();
@@ -38,46 +43,47 @@ app.get("/getSampleText4Tests", function(req, res1) {
             data += chunk;
         });
         res.on('close', () => {
- 	        console.log('Sample text: ' + data);
+            console.log('Sample text: ' + data);
             res1.setHeader('Content-Type', 'application/json');
-            res1.end(JSON.stringify({"SampleText":data}));
+            res1.end(JSON.stringify({
+                "SampleText": data
+            }));
         });
-        
+
     });
 });
 
 function returnDate() {
-let date_ob = new Date();
-var miliseconds = date_ob.getMilliseconds()
-var miliseconds = date_ob.getMilliseconds()
-if (miliseconds <= 9) {
-miliseconds = '00' + miliseconds
-};
-if (miliseconds <= 99 && miliseconds >= 10) {
-miliseconds = '0' + miliseconds
-};
-var seconds = date_ob.getSeconds();
-if (seconds <= 9) {
-seconds = '0'+seconds;
-};
-var minutes = date_ob.getMinutes();
-if (minutes <= 9) {
-minutes = '0'+minutes;
-};
-var hour = date_ob.getHours();
-if (hour <= 9) {
-hour = '0'+hour;
-};
-var year = date_ob.getFullYear();
-var month = date_ob.getMonth()+1;
-if (month <= 9) {
-month = '0'+month;
-};
-var day = date_ob.getDate();
-if (day <= 9) {
-day = '0'+day;
-};
-return day+'.'+month+'.'+year+' '+hour+':'+minutes+":"+seconds+":"+miliseconds;
+    let date_ob = new Date();
+    var miliseconds = date_ob.getMilliseconds()
+    if (miliseconds <= 9) {
+        miliseconds = '00' + miliseconds
+    };
+    if (miliseconds <= 99 && miliseconds >= 10) {
+        miliseconds = '0' + miliseconds
+    };
+    var seconds = date_ob.getSeconds();
+    if (seconds <= 9) {
+        seconds = '0' + seconds;
+    };
+    var minutes = date_ob.getMinutes();
+    if (minutes <= 9) {
+        minutes = '0' + minutes;
+    };
+    var hour = date_ob.getHours();
+    if (hour <= 9) {
+        hour = '0' + hour;
+    };
+    var year = date_ob.getFullYear();
+    var month = date_ob.getMonth() + 1;
+    if (month <= 9) {
+        month = '0' + month;
+    };
+    var day = date_ob.getDate();
+    if (day <= 9) {
+        day = '0' + day;
+    };
+    return day + '.' + month + '.' + year + ' ' + hour + ':' + minutes + ":" + seconds + ":" + miliseconds;
 }
 
 const webserver = app.listen(port, function() {
